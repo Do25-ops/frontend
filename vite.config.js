77e34d059578e5354d3d7376dc1dbd122a579d0f
Do25-ops <dataodyssey25@gmail.com>
2025-02-17 11:03:12 +0500

    Update vite.config.js
    Added api url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8800',
+        target: 'https://backend-nur7ybavd-do25-ops-projects.vercel.app',
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
     },
