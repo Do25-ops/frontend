dcf0cd31ef1c638218292c29ab901e7e88feb83a
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-17 16:34:05 +0500

    socket issue

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://backend-theta-two-99.vercel.app/',
-        //rewrite: (path) => path.replace(/^\/api/, ''), 
+        rewrite: (path) => path.replace(/^\/api/, ''), 
       },
     },
   },
