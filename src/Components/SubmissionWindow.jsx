b2ef13663ded38e5449c6feb50edb8075b20bb04
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-19 00:47:36 +0500

    fixed query fetching

--- src/Components/SubmissionWindow.jsx
+++ src/Components/SubmissionWindow.jsx
@@ -31,9 +31,8 @@ const SubmissionWindow = ({ query,dialect, toggleWindow,toggledSelected, setCanS
       const response = await axios.get(
         `${import.meta.env.VITE_BACKEND_URL}/api/getSubmissionStatus`,
         {
-          withCredentials : true
-        },
-        {
+          withCredentials : true,
+
           params: { team_id: user.team_id, query: query }, 
         }
       );
