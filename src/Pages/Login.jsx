56aa4490054e2372454763e39a651073b58b4a11
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-19 09:35:09 +0500

    final deploy

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -22,7 +22,8 @@ const Login = () => {
     if(user.level > 8) {
       setShowPopup(true);
     }
-    else if(user.loggedIn) navigate("/upcomingCompetition");
+    else if(user.loggedIn) navigate("/competition");
+    // if(user.loggedIn) navigate("/upcomingCompetition");
   },[user,user.loggedIn])
 
   const handleSubmit = async (e) => {
